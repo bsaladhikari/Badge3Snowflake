385be3119e6bd350d31a64b0286c3388264ad0d4
bsaladhikari <134571816+bsaladhikari@users.noreply.github.com>
2024-05-18 20:27:06 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,11 +32,15 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 # Fetching fruit options from Snowflake
 try:
-    cursor.execute("SELECT FRUIT_NAME FROM smoothies.public.fruit_options")
-    fruit_options = [row['FRUIT_NAME'] for row in cursor.fetchall()]
+    cursor.execute("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.public.fruit_options")
+    fruit_options = [(row['FRUIT_NAME'], row['SEARCH_ON']) for row in cursor.fetchall()]
 except Exception as e:
     st.error(f"Error: Failed to fetch fruit options: {str(e)}")
-    st.stop()
+    
+
+pd_df= fruit_options.to_pandas()
+#st.dataframe(pd_df)
+
 
 # Multiselect for ingredients
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', fruit_options)
@@ -45,6 +49,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, ',')
+        
         st.subheader(fruit_chosen+ 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
