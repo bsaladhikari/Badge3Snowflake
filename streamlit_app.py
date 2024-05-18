3c839ceddb4b484491889a4bce34728329f06efa
bsaladhikari <134571816+bsaladhikari@users.noreply.github.com>
2024-05-18 22:45:04 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
-         st.success('Your Smoothie is ordered!', icon= "✅")
+    st.success('Your Smoothie is ordered!', icon= "✅")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
 
